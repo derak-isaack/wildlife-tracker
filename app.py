5a4e7dadb9b7342e5664e70d2b9a00bcc68c90e2
deerak-isaack <oderaisaack@gmail.com>
2024-03-24 02:16:34 +0300

    model deployment

--- app.py
+++ app.py
@@ -26,35 +26,29 @@ import torch
 import streamlit as st 
 from ultralytics import YOLO
 
-device = torch.device('cpu')
-device.load_state_dict(torch.load('wildlife5_vision.pth', map_location=device))
-state_dict = torch.load('wildlife5_vision.pth', map_location=device)
-# Load the model state dictionary
+# device = torch.device('cpu')
+# device.load_state_dict(torch.load('wildlife5_vision.pth', map_location=device))
+# state_dict = torch.load('wildlife5_vision.pth', map_location=device)
+# # Load the model state dictionary
 
 
-# Instantiate the YOLO model
-model = YOLO()
+# # Instantiate the YOLO model
+model = YOLO('yolov9c.pt')
 
-# Load the state dictionary into the model
-model_state_dict = model.state_dict()  # Get the model state dictionary
-
-for key in state_dict.keys():
-    if 'model.model' in key:
-        new_key = key.replace('model.model.', '')  # Remove the redundant 'model.model' prefix
-        model_state_dict[new_key] = state_dict[key]
-model.load_state_dict(model_state_dict)
+# # Load the state dictionary into the model
+# model_state_dict = model.state_dict()  # Get the model state dictionary
 
 def main():
     st.title('Wildlife5 Vision')
     st.write('Wildlife5 Vision')
     
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
     if uploaded_file is not None:
         # Convert the uploaded file to bytes
         image_bytes = uploaded_file.getvalue()
         
         # Perform object detection on the uploaded image
-        results = model(image_bytes)
+        results = model.track(source=image_bytes, show=True, save=True)
         
         # Display the results
         st.image(results.render(), caption="Detected Objects", use_column_width=True)
