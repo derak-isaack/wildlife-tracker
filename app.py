460e1f2676605babbcc9b1a876a8fa3c69b6f053
deerak-isaack <oderaisaack@gmail.com>
2024-03-26 03:59:50 +0300

    YoloV9 training

--- app.py
+++ app.py
@@ -1,64 +1,39 @@
-# import torch
-# import streamlit as st 
-# from ultralytics import YOLO 
-
-
-# device = torch.device('cpu')
-# device.load_state_dict(torch.load('wildlife5_vision.pth', map_location=device))
-# state_dict = torch.load('wildlife5_vision.pth', map_location=device)
-# # print(state_dict.keys())
-
-# # st.header('Wildlife5 Vision')
-# # st.write('Wildlife5 Vision')
-
-
-# def main():
-#     st.title('Wildlife5 Vision')
-#     st.write('Wildlife5 Vision')
-    
-#     model = state_dict
-#     # Load a model
-    
-#     model.track(source="elephant-4736008.jpg", show=True, save=True)  # track an image
-    
-#     st.file_uploader
-
+import torch
 import streamlit as st 
-from ultralytics import YOLO
 
-# device = torch.device('cpu')
-# device.load_state_dict(torch.load('wildlife5_vision.pth', map_location=device))
-# state_dict = torch.load('wildlife5_vision.pth', map_location=device)
-# # Load the model state dictionary
+def load_model(model_path):
+    device = torch.device('cpu')
+    model = torch.load(model_path, map_location=device)
+    return model
 
+#Load the best model from trained weights in local machine  
+load_model = load_model('train6/weights/best.pt')
 
-# # Instantiate the YOLO model
-model = YOLO('yolov9c.pt')
+#Define function for detecting animals.
+def predict(image):
+    with torch.no_grad():
+        output = load_model(image)
+    return output
 
-# # Load the state dictionary into the model
-# model_state_dict = model.state_dict()  # Get the model state dictionary
-
+#Define main function for deployment   
 def main():
-    st.title('Wildlife5 Vision')
-    st.write('Wildlife5 Vision')
-    
-    uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
+    st.title("Image Detection")
+    st.write("This is a simple image Detection web application.")
+
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
     if uploaded_file is not None:
-        # Convert the uploaded file to bytes
-        image_bytes = uploaded_file.getvalue()
-        
-        # Perform object detection on the uploaded image
-        results = model.track(source=image_bytes, show=True, save=True)
-        
-        # Display the results
-        st.image(results.render(), caption="Detected Objects", use_column_width=True)
+        image = uploaded_file.read()
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
-if __name__ == "__main__":
+        if st.button("Detect"):
+            output = predict(image)
+            st.write("Predicted class:", output)
+
+
+if __name__ == '__main__':
     main()
-
     
 
 
-if __name__== 'main':
-    run(main)
 
+    
